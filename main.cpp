ad4da7fddd56278c629f1738f9e198031ca763d6
yoshizawa-katsuya <k023g0036@g.neec.ac.jp>
2024-03-15 08:04:01 +0900

    完成

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <Novice.h>
+#include "Matrix.h"
 
 const char kWindowTitle[] = "LC1A_27_ヨシザワ_カツヤ";
 
@@ -11,6 +12,11 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	// キー入力結果を受け取る箱
 	char keys[256] = {0};
 	char preKeys[256] = {0};
+
+	Vector3 scale{ 1.2f, 0.79f, -2.1f };
+	Vector3 rotate{ 0.4f, 1.43f, -0.8f };
+	Vector3 translate{ 2.7f, -4.15f, 1.57f };
+	Matrix4x4 worldMatrix = MakeAffineMatrix(scale, rotate, translate);
 
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
@@ -32,6 +38,8 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓描画処理ここから
 		///
+
+		MatrixScreenPrintf(0, 0, worldMatrix, "worldMatrix");
 
 		///
 		/// ↑描画処理ここまで
